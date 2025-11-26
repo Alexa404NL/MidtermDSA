fe10003f968a90cda5393329a7af074fca7c555d
Alexa404NL <66581333+Alexa404NL@users.noreply.github.com>
2025-11-26 11:22:35 -0600

    renato arreglo el trie :party

--- main.cpp
+++ main.cpp
@@ -1,46 +1,77 @@
 #include <iostream>
 #include <string>
 #include <sstream>
-#include <string>
 #include <vector>
-#include <algorithm>
-#include <map>
 #include <unordered_map>
-#include <queue>
+
 using namespace std;
 
-
-
 class Trie {
-
-    public: 
-        unordered_map<string, Trie*> child;
-        bool isLeaf;
+public:
+    unordered_map<string, Trie*> child;
+    bool isLeaf;
 
         Trie() {
             isLeaf = false;
         }
 
-        void insert(Trie* root, string key);
-};
-
-
-
-void insert(Trie* root, const string& key) {
-  
-    Trie* curr = root;
-    stringstream ss; 
-    string word="";
-    while(ss>>word) {
-        if (curr->child[word] == nullptr) {
-          
-            Trie* newNode = new Trie();
-          
-            curr->child[word] = newNode;
+    ~Trie() {
+        for (auto& pair : child) {
+            delete pair.second;
         }
-      
-        curr = curr->child[word];
     }
 
-    curr->isLeaf = true;
-}
+    void insert(const string& key) {
+        Trie* curr = this;
+        stringstream ss(key);
+        string word;
+        while (ss >> word) {
+            if (curr->child.find(word) == curr->child.end()) {
+                curr->child[word] = new Trie();
+            }
+            curr = curr->child[word];
+        }
+        curr->isLeaf = true;
+    }
+
+    bool contains(const string& key) {
+        Trie* curr = this;
+        stringstream ss(key);
+        string word;
+        while (ss >> word) {
+            if (curr->child.find(word) == curr->child.end()) {
+                return false;
+            }
+            curr = curr->child[word];
+        }
+        return curr != nullptr && curr->isLeaf;
+    }
+
+    void remove(const string& key) {
+        remove(this, key);
+    }
+
+private:
+    bool remove(Trie* root, const string& key) {
+        string ss(key);
+        Trie* curr= root; 
+        if (curr == nullptr) {
+            return false;
+        }
+
+        string word;
+        if (!(ss.find(' '))) {
+            // End of the key
+            if (curr->isLeaf) {
+                curr->isLeaf = false;
+                return curr->child.empty();
+            }
+        }
+
+        if (curr->child.count(word) && remove(curr->child[word], ss)) {
+            delete curr->child[word];
+            curr->child.erase(word);
+            return !curr->isLeaf && curr->child.empty();
+        }
+    }
+};
