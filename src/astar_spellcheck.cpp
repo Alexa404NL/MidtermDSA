562e10d16aa835938a3b00efa37f13e1b5253660
Realex78 <36749154+Realex78@users.noreply.github.com>
2025-12-04 19:23:09 +0000

    Pararellize Levenshtein distance

--- src/astar_spellcheck.cpp
+++ src/astar_spellcheck.cpp
@@ -1,4 +1,5 @@
 #include "../include/astar_spellcheck.h"
+#include <omp.h>
 
 AStarSpellChecker::AStarSpellChecker(Trie* t) : trie(t) {}
 
@@ -16,22 +17,31 @@ int AStarSpellChecker::heuristic(const string& current, int targetIndex, const s
 int AStarSpellChecker::levenshteinDistance(const string& s1, const string& s2) {
     int m = s1.length();
     int n = s2.length();
-    
+
+    if (m == 0) return n;
+    if (n == 0) return m;
+
     vector<vector<int>> dp(m + 1, vector<int>(n + 1));
-    
+
     for (int i = 0; i <= m; i++) dp[i][0] = i;
     for (int j = 0; j <= n; j++) dp[0][j] = j;
-    
-    for (int i = 1; i <= m; i++) {
-        for (int j = 1; j <= n; j++) {
-            if (s1[i-1] == s2[j-1]) {
-                dp[i][j] = dp[i-1][j-1];
+
+    for (int k = 2; k <= m + n; k++) {
+        int start_i = max(1, k - n);
+        int end_i = min(m, k - 1);
+
+        #pragma omp parallel for
+        for (int i = start_i; i <= end_i; i++) {
+            int j = k - i;
+            
+            if (s1[i - 1] == s2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1];
             } else {
-                dp[i][j] = 1 + min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]});
+                dp[i][j] = 1 + min({dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]});
             }
         }
     }
-    
+
     return dp[m][n];
 }
 
